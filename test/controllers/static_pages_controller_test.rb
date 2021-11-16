27fa691552c15ff29bfe99e92f8eccb338761619
Ubuntu <aniki.gt007@gmail.com>
2021-11-16 12:19:08 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutotial Sample App"
   end
 
   test "should get help" do
