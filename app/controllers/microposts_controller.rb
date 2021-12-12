11f7bb3d43d2ddcfdce1f2f9d4f99b4e967e7659
Ubuntu <aniki.gt007@gmail.com>
2021-12-12 02:08:19 +0000

    13.3.2クリア

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,9 +2,23 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)          #新しいマイクロポストをbuildするためにUser/Micropost関連付けを使う
+    if @micropost.save                                                    
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
-  
+
+  private
+
+# micropost_paramsでStrong Parametersを使っていることにより、マイクロポストのcontent属性だけがWeb経由で変更可能
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
+
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 # HTMLの各ページのコントロール
 class StaticPagesController < ApplicationController
   def home
+    @micropost = current_user.microposts.build if logged_in?        #current_userメソッドはユーザーがログインしているとき
   end
 
   def help
