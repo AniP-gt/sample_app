b18d8a30b9686568ffd453733232ead6829950bd
Ubuntu <aniki.gt007@gmail.com>
2021-12-12 12:45:44 +0000

    13.4.3クリア

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,10 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }                             #あるスコープをモデルのすべてのクエリに適用したい場合、モデル自身の内部でdefault_scopeメソッド desc（降順） => 新しい投稿順に並び替える
   validates :user_id, presence: true                                        #マイクロポストのuser_idに対する検証
   validates :content, presence: true, length: { maximum: 140 }              #content属性の検証　140文字まで　
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],       #文字列の配列を簡単に作れる%wメソッド
+                                      message: "must be a valid image format" },    #
+                      size:         { less_than: 5.megabytes,                       #比較文　
+                                      message: "should be less than 5MB" }  
 end
 
 
